c196a87ec798c57b9418763d17121daac8338bfc
desmond.obrien <des@Dess-MacBook-Air.local>
2023-02-18 16:51:00 -0700

    can pull series metadata amd region metadata

---
+++ SeriesFormatter.py
@@ -0,0 +1,38 @@
+import wbgapi.series as wseries
+import wbgapi.series_metadata as wsm
+import wbgapi.time as wtime
+import wbgapi.source as sources
+import wbgapi.data as wdata
+import json
+import pandas as pd
+import numpy as np
+
+class SerialFormatter:
+
+    def __init__(self):
+        self._series = None
+        self._df = None
+
+    def set_series(self, series: list) -> None:
+        self._series = series
+
+
+    #def pull_series(self):
+     #   df = pd.DataFrame()
+      #  for i in self._series:
+
+
+
+
+
+
+def main():
+
+    #sf = SerialFormatter()
+
+    s1 =
+
+if __name__ == "__main__":
+    main()
+
+
